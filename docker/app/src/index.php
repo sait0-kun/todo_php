8ebe75b7589db691b4caa7fce1e374d6e76852c2
sait0-kun <kitsit.39@gmail.com>
2020-08-13 18:41:23 +0900

    [add]Create login.php

--- docker/app/src/index.php
+++ docker/app/src/index.php
@@ -1,13 +1,32 @@
+<?php
+    session_start();
+    require('dbconnect.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>php7.4-apache</title>
+    <title>my-todo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-<?php require('dbconnect.php');?>
-<?php phpinfo(); ?>
+<header>todo管理アプリケーション</header>
+<section>
+    <h1>ログインする</h1>
+    <form action="" method="post">
+        <input type="text" name="user_name" size="30" maxlength="20">
+        <p>* ユーザーネームをご記入ください</p>
+        <input type="text" name="user_pass" size="30" maxlength="20">
+        <p>* パスワードをご記入ください</p>
+        <div>
+            <a href="signUp.php">新規作成</a>
+            <input type="submit" value="ログイン">
+        </div>
+    </form>
+</section>
+
+
 </body>
 </html>

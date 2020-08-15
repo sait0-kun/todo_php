5a44c50f1e31bd0f0a32a7160f54ef3d3704bb16
sait0-kun <kitsit.39@gmail.com>
2020-08-15 17:34:43 +0900

    [update]Comment out

--- docker/app/src/create_user.php
+++ docker/app/src/create_user.php
@@ -3,32 +3,39 @@
     require('dbconnect.php');
 
     if (!empty($_POST)) {
+        // ユーザー名が空白の場合
         if ($_POST['user_name'] === '') {
             $error['user_name'] = 'blank';
         }
+        // パスワードが3文字以下の場合
         if (strlen($_POST['user_pass']) < 4) {
             $error['user_pass'] = 'length';
         }
+        // パスワードが空欄の場合
         if ($_POST['user_pass'] === '') {
             $error['user_pass'] = 'blank';
         }
+        // パスワードの入力が2回目で間違っていた場合
         if ($_POST['user_pass'] !== $_POST['user_pass_re-enter']) {
             $error['user_pass_re-enter'] = 'difference';
         }
     }
 
     if (empty($error)) {
+        // ユーザー名が既に使用されている場合の処理
         $member = $db->prepare('SELECT COUNT(*) AS cnt FROM users WHERE user_name=?');
         $member->execute(array($_POST['user_name']));
         $record = $member->fetch();
         if ($record['cnt'] > 0) {
             $error['user_name'] = 'duplicate';
         } else {
+            // 問題なければセッションに設定
             $_SESSION['create'] = $_POST;
         }
     }
 
     if(!empty($_SESSION)) {
+        // ユーザー登録処理
         $statement = $db->prepare('INSERT INTO users SET user_name=?, password=?');
         $statement->execute(array(
             $_SESSION['create']['user_name'],

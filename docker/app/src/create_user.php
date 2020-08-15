65ac45e478d2d70887cea4574cb4a81ea9c9b314
sait0-kun <kitsit.39@gmail.com>
2020-08-15 13:48:28 +0900

    [update]User information error check

--- docker/app/src/create_user.php
+++ docker/app/src/create_user.php
@@ -1,6 +1,18 @@
 <?php
     session_start();
     require('dbconnect.php');
+
+    if (!empty($_POST)) {
+        if ($_POST['user_name'] === '') {
+            $error['user_name'] = 'blank';
+        }
+        if (strlen($_POST['user_pass']) < 4) {
+            $error['user_pass'] = 'length';
+        }
+        if ($_POST['user_pass'] === '') {
+            $error['user_pass'] = 'blank';
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,8 +30,17 @@
     <h1 class="create__title">新規ユーザー登録</h1>
     <form class="create__form" action="" method="post">
         <input class="create__form__input"  type="text" name="user_name" size="30" maxlength="20" placeholder="ユーザー名">
-        <input class="create__form__input"  type="text" name="user_pass" size="30" maxlength="20" placeholder="パスワード">
-        <input class="create__form__input"  type="text" name="user_pass" size="30" maxlength="20" placeholder="パスワードを再度入力して下さい">
+        <?php if ($error['user_name'] === 'blank'): ?>
+        <p>*ユーザー名を入力して下さい。</p>
+        <?php endif; ?>
+        <input class="create__form__input"  type="password" name="user_pass" size="30" maxlength="20" placeholder="パスワード">
+        <input class="create__form__input"  type="password" name="user_pass" size="30" maxlength="20" placeholder="パスワードを再度入力して下さい">
+        <?php if ($error['user_pass'] === 'length'): ?>
+        <p>*パスワードは4文字以上を入力して下さい。</p>
+        <?php endif; ?>
+        <?php if ($error['user_pass'] === 'blank'): ?>
+        <p>*パスワードを入力して下さい。</p>
+        <?php endif; ?>
         <div class="create__form__btn">
             <input class="create__form__btn-register" type="submit" value="ユーザー登録">
         </div>

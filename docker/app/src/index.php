f673a5450d1976b16a74224085ee827f56886590
sait0-kun <kitsit.39@gmail.com>
2020-08-15 11:21:29 +0900

    [add]Comment out

--- docker/app/src/index.php
+++ docker/app/src/index.php
@@ -3,8 +3,10 @@
     require('dbconnect.php');
 
     if (!empty($_POST)) {
+        // ログイン失敗時のユーザー名formに表示するため
         $user_name = $_POST['user_name'];
 
+        // 両方のformに入力された場合
         if ($_POST['user_name'] !== '' && $_POST['user_pass'] !== '') {
             $login = $db->prepare('SELECT * FROM users WHERE user_name=? AND password=?');
             $login->execute(array(
@@ -12,18 +14,22 @@
                 $_POST['user_pass'],
                 // sha1($_POST['user_pass'])
             ));
+            // ユーザー登録済みならDBから取得
             $member = $login->fetch();
 
             if ($member) {
+                // ログイン時にセッションに保存
                 $_SESSION['id'] = $member['user_id'];
                 $_SESSION['time'] = time();
 
                 header('Location: main.php');
                 exit();
             } else {
+                // ユーザー名、パスワードのどちらかを間違えた場合
                 $error['login'] = 'failed';
             }
         } else {
+            // formのどちらか一方が未入力の場合
             $error['login'] = 'blank';
         }
     }
@@ -44,13 +50,17 @@
     <h1 class="login__title">todoリストにログイン</h1>
     <form class="login__form" action="" method="post">
         <input class="login__form__input"  type="text" name="user_name" size="30" maxlength="20" placeholder="ユーザー名" value="<?php echo htmlspecialchars($user_name); ?>">
+        <!-- ログイン失敗時 -->
         <?php if ($error['login'] === 'blank'): ?>
         <p>*ユーザー名とパスワードをご記入ください。</p>
         <?php endif; ?>
+        <!--  -->
         <input class="login__form__input"  type="password" name="user_pass" size="30" maxlength="20" placeholder="パスワード">
+        <!-- ログイン失敗時 -->
         <?php if ($error['login'] === 'failed'): ?>
-        <p>*ログインに失敗しました。パスワードを正しくご記入ください。</p>
+        <p>*ログインに失敗しました。正しくご記入ください。</p>
         <?php endif; ?>
+        <!--  -->
         <div class="login__form__btn">
             <a class="login__form__btn-left" href="signUp.php">ユーザー登録</a>
             <input class="login__form__btn-right" type="submit" value="ログイン">

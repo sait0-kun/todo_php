b52b70163c98953607a2af68a45c12a32b0e7286
sait0-kun <kitsit.39@gmail.com>
2020-08-17 16:05:33 +0900

    [update]Initial data addition

--- docker/db/initdb.d/init.sql
+++ docker/db/initdb.d/init.sql
@@ -8,7 +8,7 @@ PRIMARY KEY (user_id));
 CREATE TABLE task(
 user_id INTEGER NOT NULL,
 task_id INTEGER NOT NULL AUTO_INCREMENT,
-task_name VARCHAR(100) NOT NULL,
+task_name TEXT NOT NULL,
 priority VARCHAR(1),
 PRIMARY KEY (task_id));
 
@@ -16,8 +16,21 @@ INSERT INTO users VALUES ('1', 'admin_a', SHA1('admin_a'));
 INSERT INTO users VALUES ('2', 'admin_b', SHA1('admin_b'));
 INSERT INTO users VALUES ('3', 'admin_c', SHA1('admin_c'));
 
-INSERT INTO task VALUES ('1', null, 'ra-men', '1');
-INSERT INTO task VALUES ('1', null, 'tukemen', '3');
-INSERT INTO task VALUES ('1', null, 'boku', '2');
-INSERT INTO task VALUES ('1', null, 'ikemen', '3');
+INSERT INTO task VALUES ('1', null, 'ラーメン', '1');
+INSERT INTO task VALUES ('1', null, 'つけ麺', '3');
+INSERT INTO task VALUES ('1', null, '僕', '2');
+INSERT INTO task VALUES ('1', null, 'イケメン', '3');
+INSERT INTO task VALUES ('1', null, 'スタッフ', '3');
+INSERT INTO task VALUES ('1', null, 'どーも狩野です。', '3');
+INSERT INTO task VALUES ('1', null, '読書', '3');
+INSERT INTO task VALUES ('1', null, '昼寝', '1');
+INSERT INTO task VALUES ('1', null, '昼寝', '1');
+INSERT INTO task VALUES ('1', null, '昼寝', '1');
+INSERT INTO task VALUES ('1', null, '昼寝', '1');
+INSERT INTO task VALUES ('1', null, '昼寝', '1');
+INSERT INTO task VALUES ('1', null, '昼寝', '1');
+INSERT INTO task VALUES ('1', null, '昼寝', '1');
+INSERT INTO task VALUES ('2', null, 'プログラミング', '1');
+INSERT INTO task VALUES ('2', null, 'プログラミング', '3');
+INSERT INTO task VALUES ('2', null, 'プログラミング', '2');
 

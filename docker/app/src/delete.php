b66319e8d5282921f1ab8f80fb401223da744ba3
sait0-kun <kitsit.39@gmail.com>
2020-08-17 20:05:40 +0900

    [fix]Same bug fix as update.php

--- docker/app/src/delete.php
+++ docker/app/src/delete.php
@@ -9,10 +9,17 @@
     }
 
     // タスク名の取得処理
-    if (!empty($_GET['id'])) {
-        $statement = $db->prepare('SELECT task_id, task_name FROM task WHERE task_id    ="'. $_GET['id'] .'"');
+    if (isset($_GET['id'])) {
+        $statement = $db->prepare('SELECT task_id, task_name FROM task WHERE task_id    ="'. $_GET['id'] .'" AND user_id = "'. $_SESSION['id'] .'"');
         $statement->execute();
         $task = $statement->fetch();
+
+        // 別ユーザのタスクにアクセスしたらmain.phpに戻す
+        if (empty($task)) {
+            header('Location: main.php');
+        }
+    } else {
+        header('Location: main.php');
     }
 
     if (!empty($_POST['delete'])) {

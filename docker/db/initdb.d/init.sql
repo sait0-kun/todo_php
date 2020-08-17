4cd349a8536633e4700a5ed209c5f2833d1e1305
sait0-kun <kitsit.39@gmail.com>
2020-08-18 06:33:21 +0900

    [update]Initial data change

--- docker/db/initdb.d/init.sql
+++ docker/db/initdb.d/init.sql
@@ -1,7 +1,7 @@
 USE taskApp;
 CREATE TABLE users(
 user_id INTEGER AUTO_INCREMENT,
-user_name VARCHAR(20) NOT NULL UNIQUE,
+user_name VARCHAR(20)  NOT NULL UNIQUE,
 password VARCHAR(100) NOT NULL,
 PRIMARY KEY (user_id));
 
@@ -9,28 +9,19 @@ CREATE TABLE task(
 user_id INTEGER NOT NULL,
 task_id INTEGER NOT NULL AUTO_INCREMENT,
 task_name TEXT NOT NULL,
-priority VARCHAR(1),
 PRIMARY KEY (task_id));
 
-INSERT INTO users VALUES ('1', 'admin_a', SHA1('admin_a'));
-INSERT INTO users VALUES ('2', 'admin_b', SHA1('admin_b'));
-INSERT INTO users VALUES ('3', 'admin_c', SHA1('admin_c'));
+INSERT INTO users VALUES ('1', 'ゲスト', SHA1('guest'));
+INSERT INTO users VALUES ('2', 'admin', SHA1('admin'));
 
-INSERT INTO task VALUES ('1', null, 'ラーメン', '1');
-INSERT INTO task VALUES ('1', null, 'つけ麺', '3');
-INSERT INTO task VALUES ('1', null, '僕', '2');
-INSERT INTO task VALUES ('1', null, 'イケメン', '3');
-INSERT INTO task VALUES ('1', null, 'スタッフ', '3');
-INSERT INTO task VALUES ('1', null, 'どーも狩野です。', '3');
-INSERT INTO task VALUES ('1', null, '読書', '3');
-INSERT INTO task VALUES ('1', null, '昼寝', '1');
-INSERT INTO task VALUES ('1', null, '昼寝', '1');
-INSERT INTO task VALUES ('1', null, '昼寝', '1');
-INSERT INTO task VALUES ('1', null, '昼寝', '1');
-INSERT INTO task VALUES ('1', null, '昼寝', '1');
-INSERT INTO task VALUES ('1', null, '昼寝', '1');
-INSERT INTO task VALUES ('1', null, '昼寝', '1');
-INSERT INTO task VALUES ('2', null, 'プログラミング', '1');
-INSERT INTO task VALUES ('2', null, 'プログラミング', '3');
-INSERT INTO task VALUES ('2', null, 'プログラミング', '2');
-
+INSERT INTO task VALUES ('2', null, 'プログラミング');
+INSERT INTO task VALUES ('2', null, '筋トレ');
+INSERT INTO task VALUES ('2', null, '読書');
+INSERT INTO task VALUES ('2', null, '料理');
+INSERT INTO task VALUES ('2', null, '散歩');
+INSERT INTO task VALUES ('2', null, '仕事');
+INSERT INTO task VALUES ('2', null, '旅行');
+INSERT INTO task VALUES ('2', null, '買い物');
+INSERT INTO task VALUES ('2', null, '卵買う');
+INSERT INTO task VALUES ('2', null, '洗濯');
+INSERT INTO task VALUES ('2', null, '絵');

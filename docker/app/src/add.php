20cc8d9a4ad311003b838ddcb193b6f6b1f1e251
sait0-kun <kitsit.39@gmail.com>
2020-08-18 19:42:36 +0900

    [update]Modularization of task addition function

--- docker/app/src/add.php
+++ docker/app/src/add.php
@@ -2,18 +2,7 @@
     session_start();
     require('./modules/dbconnect.php');
     require('./modules/checkLogin.php');
-
-    // タスク追加処理
-    if (!empty($_POST['add'])) {
-        $statement = $db->prepare('INSERT INTO task SET user_id=?, task_name=?');
-        $statement->execute(array(
-            $_SESSION['id'],
-            $_POST['add']
-        ));
-
-        header('Location: main.php');
-        exit();
-    }
+    require('./modules/addTask.php');
 ?>
 <!DOCTYPE html>
 <html>

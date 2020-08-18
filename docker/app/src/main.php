d819082af8ddd0f7db972514023c68be40be461e
sait0-kun <kitsit.39@gmail.com>
2020-08-18 19:54:35 +0900

    [update]Script modularization

--- docker/app/src/main.php
+++ docker/app/src/main.php
@@ -2,25 +2,7 @@
     session_start();
     require('./modules/dbconnect.php');
     require('./modules/checkLogin.php');
-
-    // ページネーションの処理
-    $page = $_REQUEST['page'];
-    if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) {
-        $page = $_REQUEST['page'];
-    } else {
-        $page = 1;
-    }
-    $start = ($page - 1) * 5;
-    // user_idに基づいてtaskを取得
-    $statement = $db->prepare('SELECT task_id, task_name FROM task WHERE user_id ="'. $_SESSION['id'] .'" ORDER BY task_id LIMIT ?, 5');
-    $statement->bindParam(1, $start, PDO::PARAM_INT);
-    $statement->execute();
-    $task = $statement->fetchAll();
-
-    // ページネーションの最後のページを取得
-    $counts = $db->query('SELECT COUNT(*) as cnt FROM task WHERE user_id ="'. $_SESSION['id'] .'"');
-    $count = $counts->fetch();
-    $max_page = ceil($count['cnt'] / 5);
+    require('./modules/pagenation.php');
 ?>
 <!DOCTYPE html>
 <html>

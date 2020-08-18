3c2685acc127a57e100dda5c91b808185c1a25b7
sait0-kun <kitsit.39@gmail.com>
2020-08-18 20:00:31 +0900

    [update]Script modularization

--- docker/app/src/create_user.php
+++ docker/app/src/create_user.php
@@ -2,19 +2,7 @@
     session_start();
     require('./modules/dbconnect.php');
     require('./modules/userInformationCheck.php');
-
-    if (empty($error)) {
-        // ユーザー名が既に使用されている場合の処理
-        $member = $db->prepare('SELECT COUNT(*) AS cnt FROM users WHERE user_name=?');
-        $member->execute(array($_POST['user_name']));
-        $record = $member->fetch();
-        if ($record['cnt'] > 0) {
-            $error['user_name'] = 'duplicate';
-        } else {
-            // 問題なければセッションに設定
-            $_SESSION['create'] = $_POST;
-        }
-    }
+    require('./modules/userDuplicateCheck.php');
 
     if(!empty($_SESSION['create'])) {
         // ユーザー登録処理

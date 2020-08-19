5a70c17334f6988c356930baefeb490e1b77f608
sait0-kun <kitsit.39@gmail.com>
2020-08-19 18:57:08 +0900

    [fix]Class modification

--- docker/app/src/components/header.php
+++ docker/app/src/components/header.php
@@ -4,10 +4,10 @@
 
 <header class="header">
   <?php if (isset($_SESSION['name'])): ?>
-    <a class="headet__left" href="main.php">todoリスト</a>
+    <a class="header__left" href="main.php">todoリスト</a>
     <a class="header__right" href="logout.php">ログアウト</a>
   <?php else: ?>
-    <a class="headet__left" href="index.php">todoリスト</a>
+    <a class="header__left" href="index.php">todoリスト</a>
     <a class="header__right" href="index.php">ログイン</a>
   <?php endif; ?>
 </header>
